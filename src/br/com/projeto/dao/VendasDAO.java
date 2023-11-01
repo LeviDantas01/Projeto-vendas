5757c42f00ffc7cc621f826010f96834ec0b0b0e
Levi Dantas <dantaslevi@gmail.com>
2023-11-01 11:09:45 -0300

    tela de historico

--- src/br/com/projeto/dao/VendasDAO.java
+++ src/br/com/projeto/dao/VendasDAO.java
@@ -4,10 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
 import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.Clientes;
 import br.com.projeto.model.Vendas;
 
 public class VendasDAO {
@@ -57,6 +61,41 @@ public class VendasDAO {
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public List<Vendas> listarVendasPorPeriodo(LocalDate dataInicio, LocalDate dataFim) {
+        try {
+            List<Vendas> lista = new ArrayList<>();
+
+            String sql = "SELECT v.id, date_format(v.data_venda, '%d/%m/%Y') AS data_formatada, c.nome, v.total_venda, v.observacoes " +
+                    "FROM tb_vendas AS v INNER JOIN tb_clientes AS c ON(v.cliente_id = c.id) WHERE v.data_venda BETWEEN ? AND ?";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, dataInicio.toString());
+            ps.setString(2, dataFim.toString());
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Vendas vendas = new Vendas();
+                Clientes clientes = new Clientes();
+
+                vendas.setId(rs.getInt("v.id"));
+                vendas.setDataVenda(rs.getString("data_formatada"));
+                clientes.setNome(rs.getString("c.nome"));
+                vendas.setTatalVenda(rs.getDouble("v.total_venda"));
+                vendas.setObs(rs.getString("v.observacoes"));
+
+                vendas.setCliente(clientes);
+                lista.add(vendas);
+            }
+
+            return lista;
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "erro" + e);
+            return null;
         }
     }
 
